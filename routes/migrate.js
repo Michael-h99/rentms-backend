b0aab4a6090d397d67f48e3b299a253486714f2a
Michael <msarpon17@gmail.com>
2026-04-12 07:54:46 -0800

    Fix migrate: use bcryptjs instead of bcrypt

--- routes/migrate.js
+++ routes/migrate.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../utils/db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 router.get("/migrate", async (req, res) => {
   try {
